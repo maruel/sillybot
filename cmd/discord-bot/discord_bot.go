97c30116837aa5047524863114aa60037d1e8f44
Marc-Antoine Ruel <maruel@gmail.com>
2024-07-09 20:45:47 -0400

    discord-bot: turned into a meme generator

--- cmd/discord-bot/discord_bot.go
+++ cmd/discord-bot/discord_bot.go
@@ -8,6 +8,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"image"
+	"image/png"
 	"log/slog"
 	"strings"
 	"sync"
@@ -15,6 +17,10 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/maruel/sillybot"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goitalic"
+	"golang.org/x/image/font/opentype"
+	"golang.org/x/image/math/fixed"
 )
 
 // discordBot is the live instance of the bot talking to the Discord API.
@@ -28,6 +34,7 @@ type discordBot struct {
 	ig           *sillybot.ImageGen
 	mem          *sillybot.Memory
 	systemPrompt string
+	f            *opentype.Font
 	chat         chan msgReq
 	image        chan intReq
 	wg           sync.WaitGroup
@@ -35,6 +42,10 @@ type discordBot struct {
 
 // newDiscordBot opens a websocket connection to Discord and begin listening.
 func newDiscordBot(ctx context.Context, token string, verbose bool, l *sillybot.LLM, ig *sillybot.ImageGen, mem *sillybot.Memory, systPrmpt string) (*discordBot, error) {
+	f, err := opentype.Parse(goitalic.TTF)
+	if err != nil {
+		slog.Error("discord", "message", "failed decoding png", "error", err)
+	}
 	discordgo.Logger = func(msgL, caller int, format string, a ...interface{}) {
 		msg := fmt.Sprintf(format, a...)
 		switch msgL {
@@ -66,6 +77,7 @@ func newDiscordBot(ctx context.Context, token string, verbose bool, l *sillybot.
 		ig:           ig,
 		mem:          mem,
 		systemPrompt: systPrmpt,
+		f:            f,
 		chat:         make(chan msgReq, 5),
 		image:        make(chan intReq, 3),
 	}
@@ -212,13 +224,13 @@ func (d *discordBot) onMessageCreate(dg *discordgo.Session, m *discordgo.Message
 	slog.Info("discord", "event", "messageCreate", "author", m.Author.Username, "message", msg)
 	if d.l == nil {
 		if _, err := dg.ChannelMessageSend(m.ChannelID, "LLM is not enabled."); err != nil {
-			slog.Error("discord", "event", "failed posting message", "error", err)
+			slog.Error("discord", "message", "failed posting message", "error", err)
 		}
 		return
 	}
 	// Immediately signal the user that the bot is preparing a reply.
 	if err := dg.ChannelTyping(m.ChannelID); err != nil {
-		slog.Error("discord", "event", "failed posting 'user typing'", "error", err)
+		slog.Error("discord", "message", "failed posting 'user typing'", "error", err)
 		// Continue anyway.
 	}
 	req := msgReq{
@@ -236,7 +248,7 @@ func (d *discordBot) onMessageCreate(dg *discordgo.Session, m *discordgo.Message
 			Reference: &discordgo.MessageReference{MessageID: req.replyToID, ChannelID: req.channelID, GuildID: req.guildID},
 		})
 		if err != nil {
-			slog.Error("discord", "event", "failed posting message", "error", err)
+			slog.Error("discord", "message", "failed posting message", "error", err)
 		}
 	}
 }
@@ -264,7 +276,7 @@ func (d *discordBot) onInteractionCreate(dg *discordgo.Session, event *discordgo
 			n, ok := data.Options[0].Value.(string)
 			if !ok {
 				if err := d.interactionRespond(event.Interaction, "internal error"); err != nil {
-					slog.Error("discord", "event", "failed handling interaction", "error", err)
+					slog.Error("discord", "message", "failed handling interaction", "error", err)
 				}
 				return
 			}
@@ -276,26 +288,26 @@ func (d *discordBot) onInteractionCreate(dg *discordgo.Session, event *discordgo
 		}
 		c.Messages = []sillybot.Message{{Role: sillybot.System, Content: systemPrompt}}
 		if err := d.interactionRespond(event.Interaction, reply); err != nil {
-			slog.Error("discord", "event", "failed handling interaction", "error", err)
+			slog.Error("discord", "message", "failed handling interaction", "error", err)
 		}
 
 	case "image":
 		if len(data.Options) != 1 {
 			if err := d.interactionRespond(event.Interaction, "internal error"); err != nil {
-				slog.Error("discord", "event", "failed handling interaction", "error", err)
+				slog.Error("discord", "message", "failed handling interaction", "error", err)
 			}
 			return
 		}
 		msg, ok := data.Options[0].Value.(string)
 		if !ok {
 			if err := d.interactionRespond(event.Interaction, "internal error"); err != nil {
-				slog.Error("discord", "event", "failed handling interaction", "error", err)
+				slog.Error("discord", "message", "failed handling interaction", "error", err)
 			}
 			return
 		}
 		if d.ig == nil {
 			if err := d.interactionRespond(event.Interaction, "Image generation is not enabled. Restart with bot.image_gen.model set in config.yml."); err != nil {
-				slog.Error("discord", "event", "failed handling interaction", "error", err)
+				slog.Error("discord", "message", "failed handling interaction", "error", err)
 			}
 			return
 		}
@@ -307,7 +319,7 @@ func (d *discordBot) onInteractionCreate(dg *discordgo.Session, event *discordgo
 		case d.image <- req:
 		default:
 			if err := d.interactionRespond(event.Interaction, "Sorry! I have too many pending image requests. Please retry in a moment."); err != nil {
-				slog.Error("discord", "event", "failed handling interaction", "error", err)
+				slog.Error("discord", "message", "failed handling interaction", "error", err)
 			}
 			return
 		}
@@ -315,7 +327,7 @@ func (d *discordBot) onInteractionCreate(dg *discordgo.Session, event *discordgo
 			Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 		})
 		if err != nil {
-			slog.Error("discord", "event", "failed handling interaction", "error", err)
+			slog.Error("discord", "message", "failed handling interaction", "error", err)
 		}
 
 	default:
@@ -380,7 +392,7 @@ func (d *discordBot) handlePrompt(req msgReq) {
 							Reference: &discordgo.MessageReference{MessageID: replyToID, ChannelID: req.channelID, GuildID: req.guildID},
 						})
 						if err != nil {
-							slog.Error("discord", "event", "failed posting message", "error", err)
+							slog.Error("discord", "message", "failed posting message", "error", err)
 						}
 					}
 					// Remember our own answer.
@@ -400,12 +412,12 @@ func (d *discordBot) handlePrompt(req msgReq) {
 					})
 					pending = ""
 					if err != nil {
-						slog.Error("discord", "event", "failed posting message", "error", err)
+						slog.Error("discord", "message", "failed posting message", "error", err)
 					} else {
 						replyToID = msg.ID
 					}
 					if err := d.dg.ChannelTyping(req.channelID); err != nil {
-						slog.Error("discord", "event", "failed posting 'user typing'", "error", err)
+						slog.Error("discord", "message", "failed posting 'user typing'", "error", err)
 						// Continue anyway.
 					}
 				}
@@ -418,7 +430,7 @@ func (d *discordBot) handlePrompt(req msgReq) {
 
 	if err != nil {
 		if _, err = d.dg.ChannelMessageSend(req.channelID, "Prompt generation failed: "+err.Error()); err != nil {
-			slog.Error("discord", "event", "failed posting message", "error", err)
+			slog.Error("discord", "message", "failed posting message", "error", err)
 		}
 	}
 }
@@ -427,27 +439,48 @@ func (d *discordBot) handlePrompt(req msgReq) {
 func (d *discordBot) handleImage(req intReq) {
 	// Use the LLM to improve the prompt!
 	msg := req.msg
+	meme := ""
 	if d.l != nil {
-		const systemPrompt = "You are autoregressive language model that specializes in creating perfect, outstanding prompts for generative art models like Stable Diffusion. Your job is to take user ideas, capture ALL main parts, and turn into amazing prompts. You have to capture everything from the user's prompt and then use your talent to make it amazing. You are a master of art styles, terminology, pop culture, and photography across the globe. Respond only with the new prompt. Exclude article words."
+		const imagePrompt = "You are autoregressive language model that specializes in creating perfect, outstanding prompts for generative art models like Stable Diffusion. Your job is to take user ideas, capture ALL main parts, and turn into amazing prompts. You have to capture everything from the user's prompt and then use your talent to make it amazing. You are a master of art styles, terminology, pop culture, and photography across the globe. Respond only with the new prompt. Exclude article words."
 		msgs := []sillybot.Message{
-			{Role: sillybot.System, Content: systemPrompt},
+			{Role: sillybot.System, Content: imagePrompt},
 			{Role: sillybot.User, Content: req.msg},
 		}
 		if reply, err := d.l.Prompt(d.ctx, msgs); err != nil {
-			slog.Error("discord", "event", "failed to enhance prompt", "error", err)
+			slog.Error("discord", "message", "failed to enhance prompt", "error", err)
 		} else {
 			msg = reply
 		}
+		const memePrompt = "You are autoregressive language model that specializes in creating perfect, outstanding meme text. Your job is to take user ideas, capture ALL main parts, and turn into amazing meme. You have to capture everything from the user's prompt and then use your talent to make it amazing filled with sarcasm. Respond only with the new meme text. Make it as succinct as possible. Use exactly one comma. Exclude article words."
+		msgs = []sillybot.Message{
+			{Role: sillybot.System, Content: memePrompt},
+			{Role: sillybot.User, Content: req.msg},
+		}
+		var err error
+		if meme, err = d.l.Prompt(d.ctx, msgs); err != nil {
+			slog.Error("discord", "message", "failed to make meme prompt", "error", err)
+		}
+		meme = strings.Trim(meme, "\"")
 	}
+
 	// TODO: Generate multiple images when the queue is empty?
 	p, err := d.ig.GenImage(msg)
 	if err != nil {
 		c := "Image generation failed: " + err.Error()
 		if _, err = d.dg.InteractionResponseEdit(req.int, &discordgo.WebhookEdit{Content: &c}); err != nil {
-			slog.Error("discord", "event", "failed posting interaction", "error", err)
+			slog.Error("discord", "message", "failed posting interaction", "error", err)
 		}
 		return
 	}
+
+	if meme != "" {
+		if n, err := drawMemeOnImage(p, d.f, meme); err != nil {
+			slog.Error("discord", "message", "failed drawing text", "error", err)
+		} else {
+			p = n
+		}
+	}
+
 	_, err = d.dg.InteractionResponseEdit(req.int, &discordgo.WebhookEdit{
 		Files: []*discordgo.File{
 			{
@@ -458,8 +491,84 @@ func (d *discordBot) handleImage(req intReq) {
 		},
 	})
 	if err != nil {
-		slog.Error("discord", "event", "failed posting interaction", "error", err)
+		slog.Error("discord", "message", "failed posting interaction", "error", err)
 	}
+}
+
+func drawMemeOnImage(p []byte, f *opentype.Font, meme string) ([]byte, error) {
+	src, err := png.Decode(bytes.NewReader(p))
+	if err != nil {
+		return nil, fmt.Errorf("failed decoding png: %w", err)
+	}
+	img, ok := src.(*image.RGBA)
+	if !ok {
+		return nil, fmt.Errorf("unexpected image format: %T", src)
+	}
+	lines := strings.Split(meme, ",")
+	switch len(lines) {
+	case 1:
+		drawTextOnImage(img, f, 0, lines[0])
+	case 2:
+		drawTextOnImage(img, f, 0, lines[0])
+		drawTextOnImage(img, f, 100, lines[1])
+	case 3:
+		drawTextOnImage(img, f, 0, lines[0])
+		drawTextOnImage(img, f, 50, lines[1])
+		drawTextOnImage(img, f, 100, lines[2])
+	case 4:
+		drawTextOnImage(img, f, 0, lines[0])
+		drawTextOnImage(img, f, 30, lines[1])
+		drawTextOnImage(img, f, 60, lines[2])
+		drawTextOnImage(img, f, 100, lines[3])
+	default:
+		drawTextOnImage(img, f, 0, lines[0])
+		drawTextOnImage(img, f, 20, lines[1])
+		drawTextOnImage(img, f, 50, lines[2])
+		drawTextOnImage(img, f, 80, lines[3])
+		drawTextOnImage(img, f, 100, lines[4])
+	}
+	w := bytes.Buffer{}
+	if err := png.Encode(&w, img); err != nil {
+		return nil, fmt.Errorf("failed encoding png: %w", err)
+	}
+	return w.Bytes(), nil
+}
+
+func drawTextOnImage(img *image.RGBA, f *opentype.Font, top int, text string) {
+	bounds := img.Bounds()
+	w := bounds.Dx()
+	h := bounds.Dy()
+	d := font.Drawer{Dst: img, Src: image.Black}
+
+	// Do once with a size way too large, then adjust the size.
+	var err error
+	if d.Face, err = opentype.NewFace(f, &opentype.FaceOptions{Size: 1000, DPI: 72}); err != nil {
+		slog.Error("discord", "message", "failed loading typeface", "error", err)
+		return
+	}
+	textWidth := d.MeasureString(text).Round()
+	if d.Face, err = opentype.NewFace(f, &opentype.FaceOptions{Size: 1000. * float64(w) / (100. + float64(textWidth)), DPI: 72}); err != nil {
+		slog.Error("discord", "message", "failed loading typeface", "error", err)
+		return
+	}
+	textWidth = d.MeasureString(text).Round()
+	textHeight := d.Face.Metrics().Height.Ceil()
+	x := (w - textWidth) / 2
+	y := top * h / 100
+	if y < textHeight {
+		y = textHeight
+	} else if y > h-20 {
+		y = h - 20
+	}
+	// Draw a crude outline.
+	for _, o := range []image.Point{{2, 0}, {0, 2}, {-2, 0}, {0, -2}, {2, 2}, {2, -2}, {-2, 2}, {-2, -2}} {
+		d.Dot = fixed.Point26_6{X: fixed.I(x + o.X), Y: fixed.I(y + o.Y)}
+		d.DrawString(text)
+	}
+	// Draw the final text.
+	d.Src = image.White
+	d.Dot = fixed.P(x, y)
+	d.DrawString(text)
 }
 
 // msgReq is an incoming message pending to be processed.
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20
 	github.com/schollz/progressbar/v3 v3.14.4
 	github.com/slack-go/slack v0.13.0
+	golang.org/x/image v0.18.0
 	golang.org/x/sync v0.7.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -21,4 +22,5 @@ require (
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/term v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
