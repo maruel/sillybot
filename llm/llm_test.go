fac5dcdd30d2698020740c5344319a5d9fe7fb6a
Marc-Antoine Ruel <maruel@gmail.com>
2024-07-15 13:40:21 -0400

    llm: accelerate tests

--- llm/llm_test.go
+++ llm/llm_test.go
@@ -48,23 +48,18 @@ func TestLLM(t *testing.T) {
 				// high quantization.
 				t.Skip("phi-3-mini is misbehaving. TODO: investigate")
 			}
+			// I tested with Q2_K and results are unreliable.
 			quant := "Q3_K_M"
 			// Hack: naming conventions are not figured out.
 			if strings.Contains(strings.ToLower(k.Basename), "qwen") {
 				quant = strings.ToLower(quant)
 			}
-			// I tested with Q2_K and results are unreliable.
-			t.Run("Blocking", func(t *testing.T) {
-				testModelBlocking(t, k.Basename+quant)
-			})
-			t.Run("Stream", func(t *testing.T) {
-				testModelStreaming(t, k.Basename+quant)
-			})
+			testModel(t, k.Basename+quant)
 		})
 	}
 }
 
-func testModelBlocking(t *testing.T, model string) {
+func testModel(t *testing.T, model string) {
 	wd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
@@ -83,71 +78,55 @@ func testModelBlocking(t *testing.T, model string) {
 			t.Error(err2)
 		}
 	})
-	msgs := []Message{
-		{Role: System, Content: opts.SystemPrompt},
-		{Role: User, Content: "reply with \"ok chief\""},
-	}
-	got, err := l.Prompt(ctx, msgs, 1, 0.1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Work around various non-determinism.
-	if want := "ok chief"; !strings.Contains(strings.ToLower(got), want) {
-		if runtime.GOOS == "darwin" && os.Getenv("CI") == "true" && os.Getenv("GITHUB_ACTION") != "" {
-			t.Log("TODO: Figure out why macOS GitHub hosted runner return an empty string")
-		} else {
-			t.Fatalf("expected %q, got %q", want, got)
-		}
-	}
-}
 
-func testModelStreaming(t *testing.T, model string) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx := context.Background()
-	opts := Options{
-		Model:        model,
-		SystemPrompt: "You are an AI assistant. You strictly follow orders. Do not add extraneous words. Only reply with what is asked of you.",
-	}
-	l, err := New(ctx, filepath.Join(filepath.Dir(wd), "cache"), &opts, loadKnownLLMs(t))
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() {
-		if err2 := l.Close(); err2 != nil {
-			t.Error(err2)
+	t.Run("Blocking", func(t *testing.T) {
+		msgs := []Message{
+			{Role: System, Content: opts.SystemPrompt},
+			{Role: User, Content: "reply with \"ok chief\""},
+		}
+		got, err := l.Prompt(ctx, msgs, 1, 0.1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Work around various non-determinism.
+		if want := "ok chief"; !strings.Contains(strings.ToLower(got), want) {
+			if runtime.GOOS == "darwin" && os.Getenv("CI") == "true" && os.Getenv("GITHUB_ACTION") != "" {
+				t.Log("TODO: Figure out why macOS GitHub hosted runner return an empty string")
+			} else {
+				t.Fatalf("expected %q, got %q", want, got)
+			}
 		}
 	})
-	msgs := []Message{
-		{Role: System, Content: opts.SystemPrompt},
-		{Role: User, Content: "reply with \"ok chief\""},
-	}
-	words := make(chan string, 10)
-	got := ""
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		for c := range words {
-			got += c
+	t.Run("Streaming", func(t *testing.T) {
+		msgs := []Message{
+			{Role: System, Content: opts.SystemPrompt},
+			{Role: User, Content: "reply with \"ok chief\""},
 		}
-		wg.Done()
-	}()
-	err = l.PromptStreaming(ctx, msgs, 1, 0.1, words)
-	close(words)
-	wg.Wait()
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Work around various non-determinism.
-	if want := "ok chief"; !strings.Contains(strings.ToLower(got), want) {
-		if runtime.GOOS == "darwin" && os.Getenv("CI") == "true" && os.Getenv("GITHUB_ACTION") != "" {
-			t.Log("TODO: Figure out why macOS GitHub hosted runner return an empty string")
-		} else {
-			t.Fatalf("expected %q, got %q", want, got)
+		words := make(chan string, 10)
+		got := ""
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			for c := range words {
+				got += c
+			}
+			wg.Done()
+		}()
+		err = l.PromptStreaming(ctx, msgs, 1, 0.1, words)
+		close(words)
+		wg.Wait()
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
+		// Work around various non-determinism.
+		if want := "ok chief"; !strings.Contains(strings.ToLower(got), want) {
+			if runtime.GOOS == "darwin" && os.Getenv("CI") == "true" && os.Getenv("GITHUB_ACTION") != "" {
+				t.Log("TODO: Figure out why macOS GitHub hosted runner return an empty string")
+			} else {
+				t.Fatalf("expected %q, got %q", want, got)
+			}
+		}
+	})
 }
 
 func loadKnownLLMs(t *testing.T) []KnownLLM {
